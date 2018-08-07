a49e669717ba3902819761d68b7bcfc9a3fb9fa4
Danielle Skinner <drenniks@lawn-143-215-60-213.lawn.gatech.edu>
2018-08-07 12:27:20 -0400

    adding references

--- interactive_example.py
+++ interactive_example.py
@@ -38,6 +38,7 @@ annot = ax.annotate("", xy=(0,0), xytext=(20,20),textcoords="offset points",
 
 annot.set_visible(False)
 
+#Credit to ImportanceOfBeingErnest on stackoverflow ( https://stackoverflow.com/questions/7908636/possible-to-make-labels-appear-when-hovering-over-a-point-in-matplotlib )
 def update_annot(ind):
     pos = sc.get_offsets()[ind["ind"][0]]
     annot.xy = pos
@@ -45,6 +46,21 @@ def update_annot(ind):
     annot.set_text(text)
     annot.get_bbox_patch().set_facecolor(cmap(norm(x[ind["ind"][0]])))
     annot.get_bbox_patch().set_alpha(0.4)
+
+def click(event):
+    vis = annot.get_visible()
+    if event.inaxes == ax:
+        cont, ind = sc.contains(event)
+        if cont:
+            update_annot(ind)
+            annot.set_visible(True)
+            fig.canvas.draw_idle()
+            print('Pulling up plots: ', files)
+            create_collage(2000,2000, files)
+        else:
+            if vis:
+                annot.set_visible(False)
+                fig.canvas.draw_idle()
 
 def create_collage(width, height, listofimages):
 	#Credit to Hugo on stackoverflow ( https://stackoverflow.com/questions/35438802/making-a-collage-in-pil )
@@ -70,21 +86,6 @@ def create_collage(width, height, listofimages):
         x += thumbnail_width
         y = 0
     new_im.show()
-
-def click(event):
-    vis = annot.get_visible()
-    if event.inaxes == ax:
-        cont, ind = sc.contains(event)
-        if cont:
-            update_annot(ind)
-            annot.set_visible(True)
-            fig.canvas.draw_idle()
-            print('Pulling up plots: ', files)
-            create_collage(2000,2000, files)
-        else:
-            if vis:
-                annot.set_visible(False)
-                fig.canvas.draw_idle()
 
 fig.canvas.mpl_connect('button_press_event', click)
 plt.show()
